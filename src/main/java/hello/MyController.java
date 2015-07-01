43cce24b871de49fc0ade1ee03a5a7b332d9fd05
June.Kim <june.kim@daumkakao.com>
2015-07-01 21:18:19 +0900

    simple source

---
+++ src/main/java/hello/MyController.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController {
+    @RequestMapping
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="june.kim") String name,
+    Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
